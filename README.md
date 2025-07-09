01b9291ce29ad7443d5b9babd4c2be072b129add
oidouh <ychadda05@gmail.com>
2025-07-09 14:48:10 -0400

    split train and test data, starting normalize

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ml_project_calories
 Predict the calories that someone burnt during a workout, based on biological and workout data.
 Created by Yash Chadda.
+
+sources:
+https://www.geeksforgeeks.org/machine-learning/calories-burnt-prediction-using-machine-learning/
+https://www.geeksforgeeks.org/python/how-to-join-datasets-with-same-columns-and-select-one-using-pandas/
+
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+'''
+next steps:
+in dataset, change male and female strings to 0 and 1 respectively for normalizing
+try one of the ml models and evaluate performance
+'''
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -15,9 +21,26 @@ import warnings
 warnings.filterwarnings('ignore')
 
 #get the dataset 
-df = pd.read_csv('data/full_data.csv')
+df = pd.read_csv('ml_project_calories/data/full_data.csv')
+
+#remove weight and duration columns (highly correlated with calories, avoid data leakage)
+to_remove = ['Weight', 'Duration']
+df.drop(to_remove, axis=1, inplace=True)
+
 print(df.head())
-print(df.shape)
+print("full dataset shape: ", df.shape)
 
+#training and testing split
+features = df.drop(['User_ID', 'Calories'], axis=1)
+target = df['Calories'].values
 
+X_train, X_val, Y_train, Y_val = train_test_split(features, target,
+                                                  test_size=0.1,
+                                                  random_state=22)
+print("training features shape: ", X_train.shape)
+print("test features shape: ", X_val.shape) 
 
+# Normalizing the features for stable and fast training.
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_val = scaler.transform(X_val)
