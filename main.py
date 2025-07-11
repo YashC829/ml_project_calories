b05f1fa0b4faacc43eb6740e23743312125f62e1
oidouh <ychadda05@gmail.com>
2025-07-11 10:38:05 -0400

    tried SHAP analysis for xgb model; found effort, age, and temp stress are most imp features

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from xgboost import XGBRegressor
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error as mae
+import shap 
 #use anaconda -> vs code, import scikit learn
 
 import warnings
@@ -27,19 +28,10 @@ def main():
 
     print(df.head())
     print("full dataset shape: ", df.shape)
-
-    '''
-    # Correlate features with target. +1 for positive corr, 0 for none, -1 for negative corr
-    corr = df.corr()
-    sb.heatmap(corr[['Calories']].sort_values(by='Calories', ascending=False), annot=True)
-    plt.show()
-    #most correlated features: Effort, Duration, Temp_Stress, Weight_Duration, Heart_Rate, Body_Temp
-    '''
      
     #training and testing split
     features = df.drop(['Calories'], axis=1)
     target = df['Calories'].values
-
     X_train, X_val, Y_train, Y_val = train_test_split(features, target,
                                                     test_size=0.2,
                                                     random_state=22) #seed value
@@ -54,8 +46,20 @@ def main():
     #train some ml models and compare their results
     models = [LinearRegression(), XGBRegressor(),
             Lasso(), RandomForestRegressor(), Ridge()]
+    
+    #just XGB Regressor 
+    models[1].fit(X_train, Y_train) #train model
+    print(f'{models[1]}: ')
 
+    train_preds = models[1].predict(X_train) #make training predictions
+    print('Training Error: ', mae(Y_train, train_preds)) #mean absolute error in units of calories.
+
+    val_preds = models[1].predict(X_val) #make testing predictions
+    print('Validation Error: ', mae(Y_val, val_preds))
+    print()
+   
     #best models in testing: XGB Regressor and Random Forest Regressor 
+    '''
     for i in range(5):
         models[i].fit(X_train, Y_train) #train model
 
@@ -67,7 +71,28 @@ def main():
         val_preds = models[i].predict(X_val) #make testing predictions
         print('Validation Error: ', mae(Y_val, val_preds))
         print()
+    '''
     
 
 if __name__=="__main__":
     main()
+
+
+'''
+# Correlate features with target. +1 for positive corr, 0 for none, -1 for negative corr
+corr = df.corr()
+sb.heatmap(corr[['Calories']].sort_values(by='Calories', ascending=False), annot=True)
+plt.show()
+#most correlated features: Effort, Duration, Temp_Stress, Weight_Duration, Heart_Rate, Body_Temp
+'''
+
+
+'''
+#SHAP analysis for most important features in XGB model
+X_val_df = pd.DataFrame(X_val, columns=features.columns.tolist()) 
+# Initialize the SHAP explainer
+explainer = shap.Explainer(models[1])
+# Calculate SHAP values for the test set
+shap_values = explainer(X_val_df)
+shap.plots.bar(shap_values) #Plot feature importance (mean abs shap values)
+'''
