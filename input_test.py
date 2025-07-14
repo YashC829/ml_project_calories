6529b612e60cce4a7ee1d5406dfcfdb626f2dd56
oidouh <ychadda05@gmail.com>
2025-07-14 14:18:23 -0400

    started input function

---
+++ input_test.py
@@ -0,0 +1,15 @@
+#get a float from user input
+#run with terminal: python input_test.py
+
+#input we want: gender, age, weight, heart rate, body temp, duration
+
+
+#list of the data
+user_input = []
+
+
+num = float(input("Enter a gender - 0 for male, 1 for female: "))
+print(num)
+user_input.append(num)
+
+print(user_input)
