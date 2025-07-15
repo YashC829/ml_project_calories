5edf7180b5a528e3415f3f3b490900f2ffd87de6
oidouh <ychadda05@gmail.com>
2025-07-15 08:49:13 -0400

    wrote code to get all the user info in terminal

--- input_test.py
+++ input_test.py
@@ -1,8 +1,9 @@
 #get a float from user input
 #run with terminal: python input_test.py
 
-#input we want: gender, age, weight, heart rate, body temp, duration
+#input we want: gender, age, weight, heart rate, body temp, duration. need the units for last 4
 
+import numpy as np
 
 #list of the data
 user_input = []
@@ -12,4 +13,26 @@ num = float(input("Enter a gender - 0 for male, 1 for female: "))
 print(num)
 user_input.append(num)
 
-print(user_input)
+num = float(input("Enter an age: "))
+print(num)
+user_input.append(num)
+
+num = float(input("Enter a weight: "))
+print(num)
+user_input.append(num)
+
+num = float(input("Enter a heart rate: "))
+print(num)
+user_input.append(num)
+
+
+num = float(input("Enter a body temp: "))
+print(num)
+user_input.append(num)
+
+num = float(input("Enter workout duration: "))
+print(num)
+user_input.append(num)
+
+user_list = np.array(user_input)
+print(user_list)
